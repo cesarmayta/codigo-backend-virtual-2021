c459974711d54890ee3db6585c7d62dcf689c32b
cesar mayta <cesarmayta@gmail.com>
2021-07-30 20:05:01 -0500

    semana 02 día 3

---
+++ semana02/dia3/main.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+
+lstProductos = ['LAPTOP','IMPRESORA HP','SILLA GAMER']
+
+@app.route('/')
+def index():
+    name = request.args.get('n','invalido')
+    user_ip = request.remote_addr
+    context = {
+        'nombre':name,
+        'user_ip':user_ip,
+        'productos':lstProductos
+    }
+    return render_template('index.html',**context)
+
+@app.route('/productos')
+def productos():
+    context = {
+        'productos':lstProductos
+    }
+    return render_template('productos.html',**context)
+
+if __name__ == '__main__':
+    app.run(debug=True,port=5000)
+
+
