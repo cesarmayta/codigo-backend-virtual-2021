54f97d43018bb3ed8b788cd184d1006a3c318a0e
cesar mayta <cesarmayta@gmail.com>
2021-07-30 22:06:59 -0500

    dia 3 bootstrap

--- semana02/dia3/main.py
+++ semana02/dia3/main.py
@@ -1,6 +1,9 @@
 from flask import Flask,render_template,request
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+Bootstrap(app)
 
 lstProductos = ['LAPTOP','IMPRESORA HP','SILLA GAMER']
 
