91433412fc4f484e8d0ac9209e39105f3d1131c4
Cesar Mayta <cesarmayta@gmail.com>
2021-08-23 22:17:10 -0500

    cliente api

--- semana05/dia3/cliente.py
+++ semana05/dia3/cliente.py
@@ -1,7 +1,22 @@
 import requests
+import json
 url = 'http://127.0.0.1:8000/api/'
-headers = {
-    'Authorization' : 'Token 59df8326ac6703c36a565eaef61ef5abda447ab0'
-}
-r = requests.get(url,headers=headers)
-print(r.text)
+
+headers_post = {'Content-type': 'application/json'}
+usuario = input("Ingrese Usuario :")
+clave = input("Ingrese clave:")
+data_post = {'usuario':usuario,'password':clave}
+p = requests.post(url + 'login',data=json.dumps(data_post),headers=headers_post)
+
+if(p.status_code == 200):
+    response  = json.loads(p.text)
+    token_key = response['token']
+    print("tu token para mi api es :" + token_key)
+    headers = {
+        'Authorization' : 'Token ' + token_key
+    }
+    r = requests.get(url,headers=headers)
+    print(r.text)
+else:
+    print("error : " + p.text)
+
