93e8103f902f00c2d9c82dd203df178d64b774d7
Cesar Mayta <cesarmayta@gmail.com>
2021-09-02 18:42:46 -0500

    dia3

---
+++ semana06/dia3/tienda/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'tienda'
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('registro',views.registro,name='registro'),
+    path('login',views.loginUsuario,name='login'),
+    path('producto/<int:producto_id>',views.producto,name='producto'),
+    path('agregarCarrito/<int:producto_id>',views.agregarCarrito,name='agregarCarrito'),
+    path('carrito',views.carrito,name='carrito'),
+    path('eliminarProductoCarrito/<int:producto_id>',views.eliminarProductoCarrito,name="eliminarProductoCarrito"),
+    path('limpiarCarrito',views.limpiarCarrito,name='limpiarCarrito'),
+    path('pedido',views.pedido,name='pedido'),
+    path('logout',views.logout_view,name='logout')
+]
