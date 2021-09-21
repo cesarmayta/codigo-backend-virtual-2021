6f5a17f9da1be8514fde55d67b9e6c8ae75406a7
Cesar Mayta <cesarmayta@gmail.com>
2021-09-20 19:03:06 -0500

    semana09

--- semana08/dia1/entorno.js
+++
@@ -1,5 +0,0 @@
-let nombre = process.env.NOMBRE || 'sin nombre';
-let email = process.env.EMAIL || 'no tienen correo';
-
-console.log('Hola ' +  nombre);
-console.log('Email : ' + email);
--- semana08/dia2/sc_tabla_alumnos.sql
+++
@@ -1,7 +0,0 @@
---drop table alumno;
-create table alumno(
-id int primary key identity,
-nombre varchar(255) null,
-email varchar(255) null,
-pais varchar(255) null
-);
